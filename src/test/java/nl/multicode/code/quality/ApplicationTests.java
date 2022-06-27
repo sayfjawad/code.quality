b95acddcbbe379ccc215345d0ebbf93367b37c7f
sayfjawad <sayf@multicode.nl>
2022-06-28 01:39:26 +0200

    Make initial project

---
+++ src/test/java/nl/multicode/code/quality/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.multicode.code.quality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
